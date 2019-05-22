477fdf2dbe4f496a06d8723a9e2868f180eba492
pcadmin <pcadmin@INFPUMAC16.local>
2019-05-22 13:34:09 +0530

    Added summary and histogram metrics

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3
+
 from flask import Flask, Response
-from prometheus_client import Counter, Gauge, generate_latest
+from prometheus_client import Counter, Gauge, generate_latest, Summary, Histogram, start_http_server, make_wsgi_app,  start_wsgi_server
+
+
+import time
 
 
 app = Flask("__name__")
 
 
 my_counter = Counter('my_counter', 'Testing counter')
+
 my_gauge = Gauge('my_gauge', 'Testing gauge')
+
+my_summary = Summary('response_latency_seconds', 'Response latency (seconds)')
+obv_summary = Summary('request_size_bytes', 'Request size (bytes)')
+
+my_histogram = Histogram('response_latency_seconds_his', 'Response latency (seconds)')
+obv_histogram = Histogram('request_size_bytes_his', 'Request size (bytes)')
 
 
 @app.route("/counter")
@@ -18,13 +30,49 @@ def counter():
 @app.route("/gauge-inc")
 def gauge_inc():
     my_gauge.inc()
-    return "Testing gauge inc" 
+    return "Testing gauge inc"
 
 
 @app.route("/gauge-dec")
 def gauge_dec():
     my_gauge.dec()
-    return "Testing gauge dec" 
+    return "Testing gauge dec"
+
+
+@my_summary.time()
+def sleep_function():
+    """A dummy function"""
+    time.sleep(5)
+    return "Testing Summary"
+
+
+@my_histogram.time()
+def sleep_function_histogram():
+    """A dummy function"""
+    time.sleep(2)
+    return "Testing Histogram"
+
+
+@app.route("/summary")
+def summary():
+    return sleep_function()
+
+
+@app.route("/summary-observe")
+def summary_observe():
+    obv_summary.observe(102)
+    return "Testing summary-observe"
+
+
+@app.route("/histogram")
+def histogram():
+    return sleep_function_histogram()
+
+
+@app.route("/histogram-observe")
+def histogram_observe():
+    obv_histogram.observe(102)
+    return "Testing histogram-observe"
 
 
 @app.route("/metrics")
@@ -34,7 +82,8 @@ def metrics():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=9898, debug=True)
+
 
 
 # docker network create prometheus
